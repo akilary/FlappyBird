daafae5bad840475c852b0a22b4d235b01043e17
akiry <name@>
2025-01-18 00:35:16 +0500

    The base.png image has been doubled in size for easier rendering on the screen.

--- core/base.py
+++ core/base.py
@@ -15,17 +15,15 @@ class Base(pg.sprite.Sprite):
         self.rect = self.image.get_rect(bottomleft=(0,self.cfg.height))
 
         self.pos = pg.math.Vector2(self.rect.topleft)
-        self.half_width = self.rect.centerx
 
     def update(self, dt, bird_alive: bool=True) -> None:
         """Обновляет положение земли и циклично прокручивает её."""
         if bird_alive:
             self.pos.x -= self.MOVEMENT_SPEED * dt
-            if abs(self.pos.x) >= self.half_width: self.pos.x = 0
+            if self.pos.x <= -120: self.pos.x = 0
             self.rect.x = round(self.pos.x)
         self._draw()
 
     def _draw(self) -> None:
         """Отрисовывает землю на экране, создавая эффект прокрутки."""
         self.screen.blit(self.image, (self.pos.x, self.pos.y))
-        self.screen.blit(self.image, (self.pos.x + self.rect.width, self.pos.y))
